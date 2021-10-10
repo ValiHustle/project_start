0703730b363536a77c9932b7759cd141920bf322
Jorj <egor333555303@gmail.com>
2021-10-10 19:43:40 +0300

    first practic

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 " use strict";
 
-const re="reer";
-console.log(re);
+const numberOfFilms = +prompt('Сколько фильмов уже посмотрел', '');
+
+const personalMoviesD8 = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из просмотренных просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Один из просмотренных просмотренных фильмов?", ""),
+      d = prompt("На сколько оцените его?", "");
+
+personalMoviesD8.movies[a] = b;
+personalMoviesD8.movies[c] = d;
+
+console.log(personalMoviesD8);
